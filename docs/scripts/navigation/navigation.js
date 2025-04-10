3a93dadfde49c5b8382b3d63651db415488553c5
sysqo82 <sysqo82@gmail.com>
2025-04-10 19:17:02 +0100

    defaulting navigation to homepage take 4

--- docs/scripts/navigation/navigation.js
+++ docs/scripts/navigation/navigation.js
@@ -24,6 +24,7 @@ function setSelectedPage() {
 
     switch (currentPage) {
         case 'index.html':
+        case '/':    
         case '':    
             navigationSelect.value = 'group-stage';
             break;
