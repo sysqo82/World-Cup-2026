a31d02d5a66e78ded2ee6455473e6b2b2cd81765
sysqo82 <sysqo82@gmail.com>
2025-04-12 16:29:39 +0100

    adding the rest of the tournament

---
+++ docs/scripts/semi-final.js
@@ -0,0 +1,17 @@
+import { auth } from './config/firebase-config.js';
+import { generateRoundMatches } from './create-round-matches/create-round-matches.js';
+
+// Check if the user is logged in
+auth.onAuthStateChanged(user => {
+    if (user) {
+        console.log(`User is logged in: ${user.email}`);
+    } else {
+        console.log('No user is logged in.');
+    }
+});
+
+// Add event listener for navigation dropdown
+document.getElementById('navigation-select').addEventListener('change', navigateToPage);
+
+// Generate matches on page load
+document.addEventListener('DOMContentLoaded', generateRoundMatches('.semi-finals-container', 'semiFinalsTeams', 'Semi Finals'));
