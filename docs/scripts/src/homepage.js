22de28049f7ef761e11db4e6fcd7f77de0279ff1
sysqo82 <sysqo82@users.noreply.github.com>
2025-04-21 10:40:08 +0000

    deploy: 141e23c1139d5ca29b4dbac12ece767e2cb96e13

--- docs/scripts/src/homepage.js
+++
@@ -1,5 +0,0 @@
-import { initializeHomepage } from '../utils/user-utils.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initializeHomepage(); // Delegate all homepage logic to user-utils.js
-});
