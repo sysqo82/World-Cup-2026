3fe0d2ed0fc5aec6ff7b2c2d87f55bef0204e7fc
Assaf Itzikson <assaf.itzikson@immediate.co.uk>
2025-05-12 09:21:47 +0100

    removing the code until I get a better solution

--- docs/scripts/src/my-account.js
+++ docs/scripts/src/my-account.js
@@ -1,6 +1,5 @@
-import { getAssignedTeam, logoutUser, getCookie, setCookie } from '../utils/user-utils.js';
-import { isAllowed, isRegistered, basePath } from "../navigation/navigation.js";
-import { changeEmailURL } from '../config/firebase-config.js';
+import { getAssignedTeam, logoutUser } from '../utils/user-utils.js';
+import { isAllowed, isRegistered } from "../navigation/navigation.js";
 
 // Add event listener for navigation dropdown
 document.getElementById('navigation-select').addEventListener('change', navigateToPage);
@@ -10,40 +9,6 @@ document.addEventListener('DOMContentLoaded', async () => {
     await isAllowed();
 
     document.getElementById('logout').addEventListener('click', logoutUser);
-    document.getElementById('change-email').addEventListener('click', async () => {
-        const newEmail = document.getElementById('new-email').value;
-        const userEmail = getCookie("userDetails") ? JSON.parse(getCookie("userDetails")).email : null;
-
-        if (!newEmail) {
-            alert("Please enter a new email address.");
-            return;
-        }
-
-        try {
-            const response = await fetch(changeEmailURL, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ email: userEmail, newEmail }),
-            });
-
-            if (response.ok) {
-                alert("Email changed successfully.");
-                // Update the cookie with the new email
-                const userDetails = JSON.parse(getCookie("userDetails"));
-                userDetails.email = newEmail;
-
-                setCookie("userDetails", JSON.stringify(userDetails), 30);
-                window.location.href = `${basePath}/pages/my-account.html`;
-            } else {
-                alert("Error changing email. Please try again.");
-            }
-        } catch (error) {
-            console.error("Error:", error);
-            alert("Error changing email. Please try again.");
-        }
-    });
 
     await getAssignedTeam();
 });
